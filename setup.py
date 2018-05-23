0bc7dfc947b7eb1ed8cc63f410fa04e3ab40636d
marshareb <jamesmarshallreber@gmail.com>
2018-05-23 18:49:26 -0400

    Fixed setup.py and added tag

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'BotMe',
-  packages = ['BotMe'], # this must be the same as the name above
+  packages = ['BotMe'],
   version = '0.1',
   description = 'A basic library for building and maintaining GroupmeBots',
   author = 'James Marshall Reber',
   author_email = 'jamesmarshallreber@gmail.com',
-  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['GroupMe', 'bot'], # arbitrary keywords
+  url = 'https://github.com/marshareb/BotMe',
+  download_url = 'https://github.com/marshareb/BotMe/archive/0.1.tar.gz',
+  keywords = ['GroupMe', 'bot'],
   classifiers = [],
 )
