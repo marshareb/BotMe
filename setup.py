6a1cf1a0657bb078b51502b4aea64f33c3e7cbed
marshareb <jamesmarshallreber@gmail.com>
2018-05-23 19:38:59 -0400

    Fixed setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'James Marshall Reber',
   author_email = 'jamesmarshallreber@gmail.com',
   url = 'https://github.com/marshareb/BotMe',
-  download_url = 'https://github.com/marshareb/BotMe/archive/0.1.tar.gz',
+  download_url = 'https://github.com/marshareb/BotMe/archive/0.2.tar.gz',
   keywords = ['GroupMe', 'bot'],
   classifiers = [],
 )
